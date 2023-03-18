79d94f4a294e96ff529d2defbb98cd793441f68b
SHUBHANSHU MISHRA <rollsroyace360@gmail.com>
2023-03-18 10:54:16 +0530

    automatic update setting
    frame refinement will be done automatic and will be saved in server

--- factory.py
+++ factory.py
@@ -6,6 +6,17 @@ import pandas as pd  # used by main module
 import pytz
 from datetime import datetime
 
+def alfa_string():
+    return """ At Saffron.ai, we are dedicated to improving student engagement and learning outcomes through the use of cutting-edge technology. Our facial expression
+    recognition system provides real-time data on student attentiveness during class, allowing teachers to 
+    adjust their teaching approach and create more personalized and effective learning experiences for their
+      students. By integrating this data with other sources such as student feedback and curriculum data, we 
+      provide a comprehensive understanding of student progress and needs.
+      Our goal is to help educators leverage the power of technology to create more engaging and effective learning environments, leading to improved outcomes for students and a more equitable and inclusive education system. We understand the importance of creating a supportive and positive learning environment, and our system has the potential to reduce stress levels among students while improving their academic performance and long-term success.
+      We are committed to implementing our system responsibly,addressing potential ethical concerns surrounding the use of facial recognition technology, such as 
+      privacy and bias issues. We believe that by using technology in a responsible and ethical manner, we can 
+      contribute to a more equitable and inclusive education system.
+      Join us in our mission to revolutionize education and empower students to reach their full potential."""
 def final_function(model_face,frame):
     frame_gray  = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
     faces =  model_face.detectMultiScale(frame_gray)
--- main.py
+++ main.py
@@ -4,65 +4,67 @@ import cv2
 import numpy as np
 from factory import *
 from plotly import express as exp
-
-st.set_page_config(page_title="Result",layout="centered")
-
 data_cascade = cv2.CascadeClassifier("haarcascade_frontalface_alt2.xml")
-
-my_db  = mysql.connector.connect(
+# ==========================   main.pyh ===========================
+def job():
+    my_db  = mysql.connector.connect(
           host="sql.freedb.tech",
           user="freedb_images",
           password="gEzze6ZHjU#M4e2",
           database="freedb_image_data"
     )
-mycursor = my_db.cursor()
-
-mycursor.execute("SELECT * FROM images")
-
-myresult = mycursor.fetchall()
-final_list = []
-for x in myresult:
-  final_list.append(x)
-
-for i in range(len(final_list)):
+    mycursor = my_db.cursor()
   
-  image_data = final_list[i][2]
-  nparr = np.frombuffer(image_data, np.uint8)
-  try:
-    img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)   # getting the gray-scale image
-    # appying the fucntion here
-    final_function_and_save(data_cascade,img)  # emotion detection and saving refined data in database
+    mycursor.execute("SELECT * FROM images")
+    
+    myresult = mycursor.fetchall()
+
+    final_list = []
+    for x in myresult:
+        final_list.append(x)
+
+    for i in range(len(final_list)):
   
-  except:
-    continue 
+        image_data = final_list[i][2]
+        nparr = np.frombuffer(image_data, np.uint8)
+        try:
+            img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)   # getting the gray-scale image
+            # appying the fucntion here
+            final_function_and_save(data_cascade,img)  # emotion detection and saving refined data in database
+
+        except:
+            continue 
+
+    # delete all the images after data has been refined
+    mycursor = my_db.cursor()
+    sql = "TRUNCATE TABLE images"
+    mycursor.execute(sql)
+    my_db.commit()
+    mycursor.close()
+    return myresult
 
 
-# delete all the images after data has been refined
 
 
-mycursor = my_db.cursor()
-sql = "TRUNCATE TABLE images"
-mycursor.execute(sql)
-my_db.commit()
-mycursor.close()
+# ===================================================================
+
+st.set_page_config(page_title="Result",layout="centered")
+
+data_cascade = cv2.CascadeClassifier("haarcascade_frontalface_alt2.xml")
+
+
+my_db  = mysql.connector.connect(
+              host="sql.freedb.tech",
+              user="freedb_images",
+              password="gEzze6ZHjU#M4e2",
+              database="freedb_image_data"
+        )
+
 
 
 # ------------------------  main_workflow -----------------------
 with st.expander("About", expanded=False):
-  st.write("""
-  At Saffron.ai, we are dedicated to improving student 
-  engagement and learning outcomes through the use of cutting-edge technology. Our facial expression
-    recognition system provides real-time data on student attentiveness during class, allowing teachers to 
-    adjust their teaching approach and create more personalized and effective learning experiences for their
-      students. By integrating this data with other sources such as student feedback and curriculum data, we 
-      provide a comprehensive understanding of student progress and needs.
-Our goal is to help educators leverage the power of technology to create more engaging and effective learning environments, leading to improved outcomes for students and a more equitable and inclusive education system. We understand the importance of creating a supportive and positive learning environment, and our system has the potential to reduce stress levels among students while improving their academic performance and long-term success.
-
-We are committed to implementing our system responsibly,
- addressing potential ethical concerns surrounding the use of facial recognition technology, such as 
- privacy and bias issues. We believe that by using technology in a responsible and ethical manner, we can 
- contribute to a more equitable and inclusive education system.
-Join us in our mission to revolutionize education and empower students to reach their full potential.""")
+  st.write(alfa_string())
 
 # adding the link
 st.markdown("[click here for camera access](https://alternative-3znj.onrender.com/)", )
@@ -97,5 +99,15 @@ if checkbox:
     st.title("Dataframe")
     st.subheader("main dataframe")
     st.write(data_emotion)
-
+else:
+   x = True
+   while(x):
   
+      alfa = job()
+      if(len(alfa) == 0):
+         x = False
+      
+
+      
+
+    
